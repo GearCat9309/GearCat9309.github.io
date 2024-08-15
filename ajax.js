6a04efb4fcc44248a016f38e3c21fb46f3dbe263
GearCat9309 <75254566+GearCat9309@users.noreply.github.com>
2024-08-15 22:11:43 +0800

    Add files via upload

---
+++ ajax.js
@@ -0,0 +1,44 @@
+//呼叫Azure Function
+function azureAjax(parameter, callBack){
+	var azureUrl="https://onlinegame20240626204926.azurewebsites.net/api/";
+	if(document.URL.substring(0,4) == "file")
+		azureUrl="http://localhost:7035/api/";
+	$.ajax({
+		url: azureUrl+parameter,
+		timeout: 180000,
+		dataType: 'json',
+		type: "GET",
+		success: function(result){
+			var json = result;
+			if(json[0] == "Err")
+			{
+				$("#rtnMessage")[0].innerText = json[1];
+			}
+			else
+				callBack(json);
+		},
+		error: function (xhr, status, error) {
+			alert("ajax連線錯誤: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+		}
+	});
+}
+//每個指令都要檢查Token
+function checkToken(process){
+	var num = Number($("#count")[0].value);
+	//要求重新給Token
+	if(num == 10)
+	{
+		function ajaxSuccess(json){
+			$("#count")[0].value = "1";
+			$("#token")[0].value = json[1];
+			process();
+		}
+		var parameter = "TokenRenewal?ID="+$("#id")[0].value+"&Token="+$("#token")[0].value
+		azureAjax(parameter,ajaxSuccess);
+	}
+	else
+	{
+		$("#count")[0].value = num+1;
+		process();
+	}
+}
