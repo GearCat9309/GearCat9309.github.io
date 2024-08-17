dcbdc62e717c650f66bff6b072381179662883df
GearCat9309 <75254566+GearCat9309@users.noreply.github.com>
2024-08-18 00:55:01 +0800

    Add files via upload

--- WC_login.js
+++ WC_login.js
@@ -2,35 +2,31 @@ class FormSection extends HTMLElement {
 	connectedCallback() {
 	//登錄
     this.innerHTML = `
-	<div class="row justify-content-center" id="logArea">
-		<div class="col-6">
+	<div class="row" id="logArea">
+		<div class="col-12">
 			<div class="row mb-1">
-				<div class="col-3 px-1 text-center">帳號</div>
-				<div class="col-9 px-1">
+				<div class="col-2 px-1 text-center">帳號</div>
+				<div class="col-7 px-1">
 					<input type="text" class="form-control form-control-sm bg-dark text-white" id="account" maxlength="30"/>
 				</div>
 			</div>
 			<div class="row mb-1">
-				<div class="col-3 px-1 text-center">姓名</div>
-				<div class="col-9 px-1">
-					<input type="text" class="form-control form-control-sm bg-dark text-white" id="registerName" maxlength="30"/>
-				</div>
-			</div>
-		</div>
-		<div class="col-6">
-			<div class="row mb-1">
-				<div class="col-3 px-1 text-center">密碼</div>
-				<div class="col-9 px-1">
+				<div class="col-2 px-1 text-center">密碼</div>
+				<div class="col-7 px-1">
 					<input type="password" class="form-control form-control-sm bg-dark text-white" id="password" maxlength="30"/>
 				</div>
-			</div>
-			<div class="row mb-1">
-				<div class="col-12 text-end">
+				<div class="col-3 text-center">
 					<button class="btn btn-success btn-sm" onclick="login()">登錄</button> 
+				</div>
+			</div>
+			<div class="row mb-1 border-bottom border-white"></div>
+			<div class="row mb-1">
+				<div class="col-2 px-1 text-center">姓名</div>
+				<div class="col-7 px-1">
+					<input type="text" class="form-control form-control-sm bg-dark text-white" id="registerName" maxlength="30"/>
+				</div>
+				<div class="col-3 text-center">
 					<button class="btn btn-success btn-sm" onclick="register()">註冊</button>
-					<input type="text" id="id" style="display:none"/>
-					<input type="text" id="token" style="display:none"/>
-					<input type="text" id="count" style="display:none"/>
 				</div>
 			</div>
 		</div>
